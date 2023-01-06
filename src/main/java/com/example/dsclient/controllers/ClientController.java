d2c66c061537c7aace9b6e4619920e30b3e79b00
RichardBrayan22 <richardbrayan2009@gmail.com.br>
2023-01-06 20:30:53 -0300

    PUT Client

--- src/main/java/com/example/dsclient/controllers/ClientController.java
+++ src/main/java/com/example/dsclient/controllers/ClientController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,7 +39,7 @@ public class ClientController {
         return ResponseEntity.ok().body(list);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<ClientDTO>findById(@PathVariable Long id){
         ClientDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
@@ -52,4 +53,9 @@ public class ClientController {
         return ResponseEntity.created(uri).body(dto);
     }
     
+    @PutMapping(value ="/{id}")
+    public ResponseEntity<ClientDTO>update(@PathVariable Long id, @RequestBody ClientDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
 }
